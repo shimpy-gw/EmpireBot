2f6405dd45961f78b2eb3752c0d28bd38850966c
shimpy-gw <42718222+shimpy-gw@users.noreply.github.com>
2018-08-26 21:36:14 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -46,7 +46,7 @@ bot.on("message", function(message) {
             var embed = new Discord.RichEmbed()
                 .addTitle("Membres")
                 .addDescription("`8ball` - Réponses aléatoires")
-                .addDescription("`ping` - Le bot répond 'pong'"
+                .addDescription("`ping` - Le bot répond 'pong'")
                 .addField("Liens", "[Lien du serveur discord](https://discord.gg/h5aRW32)")
                 .setColor("#01AEE3")
                 .setFooter("Games World | by Shimpy#9999")
