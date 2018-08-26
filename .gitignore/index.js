11b69140c4b7723ce559edc971c0192b481dfe40
shimpy-gw <42718222+shimpy-gw@users.noreply.github.com>
2018-08-26 20:51:25 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -47,10 +47,10 @@ bot.on("message", function(message) {
                 .setTitle("Games World")
                 .setDescription("Voici la liste des commandes disponibles")
                 .setTitle("Membres")
-                .setDescription("`8ball` | Réponses aléatoires")
-                .setDescription("`ping` | Le bot répond 'pong'")
-                .setDescription("`info` | Avoir des informations")
-                .setFooter("Games World | https://discord.gg/h5aRW32")
+                .setDescription("`8ball` Réponses aléatoires")
+                .setDescription("`ping` Le bot répond 'pong'")
+                .setDescription("`info` Avoir des informations")
+                .setFooter("Vous avez une question ? | Adressez-vous à un administrateur")
                 .setColor("#01AEE3")
             message.channel.sendEmbed(embed);
     }
